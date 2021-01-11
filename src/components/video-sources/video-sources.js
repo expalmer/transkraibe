525c6184c6467a725aee160aa4d9329abf668eba
Marcelo Souza Lima <marcelo.nalon@corp.globo.com>
2021-01-10 23:44:38 -0300

    Use the selected file from input file on the video playback

--- src/components/video-sources/video-sources.js
+++ src/components/video-sources/video-sources.js
@@ -1,24 +1,37 @@
 import "./video-sources.css";
 
 export const VideoSources = ({ videos, handleSelectVideo }) => {
-  const handleSelect = (e) => {
+  const onSelectedSample = (e) => {
     const { value } = e.target;
     if (!value) return;
     const video = videos.find((x) => x.name === value);
     handleSelectVideo(video.source);
   };
 
+  const onSelectedFile = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      const objectUrl = URL.createObjectURL(selectedFile);
+      handleSelectVideo(objectUrl);
+    }
+  };
+
   return (
     <div className="video-sources">
       <label for="video-input-file" className="video-sources__select">
-        <input id="video-input-file" type="file" accept="video/mp4" />
+        <input
+          id="video-input-file"
+          type="file"
+          accept="video/mp4"
+          onChange={onSelectedFile}
+        />
         Upload a mp4 video
       </label>
       <p>or</p>
       <select
         id="video-sample-selector"
         className="video-sources__select"
-        onChange={handleSelect}
+        onChange={onSelectedSample}
       >
         <option value="">Select a sample</option>
         {videos.map(({ name }) => (
